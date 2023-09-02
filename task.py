e1f84d1acc7cbe3664c38a35f7f11d3a7a0d43b4
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2023-09-03 07:03:08 +0800

    Create task.py

---
+++ task.py
@@ -0,0 +1,39 @@
+import pymongo
+import sys
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description='下载任务管理')
+parser.add_argument("--act", help="操作类型：download,del", default="download")
+parser.add_argument("--projet", help="deta.space的project_id", default="")
+parser.add_argument("--apikey", help="deta.space的x_api_key", default="")
+parser.add_argument("--taskkey", help="要删除的任务key", default="")
+
+args = parser.parse_args()
+
+
+DETA_DATAKEY=args.apikey
+DETA_PROJECT_ID=args.projet
+
+QUERY_URL=f"https://database.deta.sh/v1/{DETA_PROJECT_ID}/pikpak_task/query"
+DELETE_URL=f"https://database.deta.sh/v1/{DETA_PROJECT_ID}/pikpak_task/{args.taskkey}"
+
+deta_headers = {
+    'X-API-Key':DETA_DATAKEY,
+    'Content-Type':'application/json'
+}
+
+if args.act=="del":
+  payload = {
+    "query": [{"isnow": 1}],
+    "limit": 1
+  }
+  tasks_req = requests.post(QUERY_URL,headers=deta_headers,data=payload,verify=False)
+  tasks=json.loads(tasks_req.text)
+  
+  quit()
+
+
+if args.act=="download":
+  quit()
+
